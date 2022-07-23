c4df11c40fdcbac21d0defb51ac584bb54f04a63
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-23 09:17:01 +0300

    Таблица кубов введенного числа N от 1 до N

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+Console.WriteLine("Введите число ");
+double number = Convert.ToInt32(Console.ReadLine());
+double j = 1;
+
+while(j <= number)
+{
+    double result = Math.Pow(j, 3);
+    System.Console.WriteLine(result);
+    j++;
+}
