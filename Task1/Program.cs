3443e028743624b8d7b0ca35fe0172c77cb61435
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-23 08:39:50 +0300

    Проверка, является ли пятизначное число палинромом

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите пятизначное число");
+int number = int.Parse(Console.ReadLine());
+
+int a1 = number%10;
+int a2 = number/10%10;
+int a3 = number/100%10;
+int a4 = number/1000%10;
+int a5 = number/10000;
+int numberReverse = int.Parse($"{a1}{a2}{a3}{a4}{a5}");
+System.Console.WriteLine($"Обратное число = {numberReverse}");
+
+if(numberReverse == number)
+{
+    System.Console.WriteLine("Обратное число является палинромом");
+}
+else System.Console.WriteLine("Обратное число не является палинромом");
